8373dcb530eaa44922c217c012209f62f5c373b4
Roman Zhurakivskyy <roman@zhr.org.ua>
2018-02-08 14:37:27 +0200

    systemd silents script's messages until its finish - making stdout unbuffered

--- RPi/wtower.py
+++ RPi/wtower.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
-import signal, os, sys
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def writelines(self, datas):
+       self.stream.writelines(datas)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+
+import sys
+sys.stdout = Unbuffered(sys.stdout) #making stdout unbuffered
+
+import signal, os
 from time import sleep
 import time
 import RPi.GPIO as GPIO
